0a3baf376c42042bdc1c03f1debd53b982efd121
AliceGarden <joanny.saillet@gmail.com>
2017-01-06 09:38:05 +0100

    ajout fonction controller log connection

--- api/controllers/LogConnexionController.js
+++ api/controllers/LogConnexionController.js
@@ -7,5 +7,26 @@
 
 module.exports = {
 	
+	getLogConnexionById: function (req,res) {
+		return res.ok(LogConnexion.findOne({
+			employeeId: req.param('employeeId')
+		}).exec(function(err,logconnexion){
+			if(logconnexion){
+				console.log(logconnexion);
+			}
+			return logconnexion;
+		}));
+	},
+	
+	addLog: function (req,res) {
+		LogConnexion.create( {employeeId :req.employeeId , message:req.message}, function function(err,created){
+			if(!err) {
+				console.log('Log créé.');
+			}
+			else {
+			   return err;
+			}
+	    });
+	}
 };
 
